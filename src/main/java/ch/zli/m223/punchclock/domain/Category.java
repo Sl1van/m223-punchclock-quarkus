a7435ae45922e71851115c168a9ad536f19c893e
Sl1van <58076574+Sl1van@users.noreply.github.com>
2021-09-28 11:44:18 +0200

    added getters/setters for the new domain objects

--- src/main/java/ch/zli/m223/punchclock/domain/Category.java
+++ src/main/java/ch/zli/m223/punchclock/domain/Category.java
@@ -18,4 +18,30 @@ public class Category {
 
     @OneToMany(mappedBy="categories")
     private List<Entry> entries;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    
 }
--- src/main/java/ch/zli/m223/punchclock/domain/User.java
+++ src/main/java/ch/zli/m223/punchclock/domain/User.java
@@ -24,4 +24,46 @@ public class User {
 
     @OneToMany(mappedBy="user")
     private List<Entry> entries;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(List<Role> role) {
+        this.role = role;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    
 }
